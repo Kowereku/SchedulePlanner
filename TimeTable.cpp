150609c15af2b6dad75e40262bc686d4b45ef1cb
coverek <pawelzambrzycki03@gmail.com>
2024-06-18 15:51:30 +0200

    Fixed misspellings

--- TimeTable.cpp
+++ TimeTable.cpp
@@ -3,12 +3,12 @@
 TimeTable::TimeTable(float width, float height, State& state) : currentState(state) // konstruktor planu wydarzen
 {
     if (!font.loadFromFile("Calligraphy.ttf")) { // ladowanie czcionek i tla
-        std:cerr << "Nie mozna zaladowac czcionki" << std::endl;
+        std::cerr << "Nie mozna zaladowac czcionki" << std::endl;
     }
 
     if (!fontTypewriter.loadFromFile("Typewriter.ttf"))
     {
-        std:cerr << "Nie mozna zaladowac czcionki" << std::endl;
+        std::cerr << "Nie mozna zaladowac czcionki" << std::endl;
     }
 
     if (!backgroundTexture.loadFromFile("tlokal_2.jpg"))
